5bc5543b871101f523169f2b9f9a9f3881c82277
Michael Evans <azmathtech@gmail.com>
2017-08-08 16:11:49 -0700

    Refactor passport and initial client set up

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }) //returns a promise
-        .then(existingUser => {
-          if (existingUser) {
-            //we already have a record with the given profile ID
-            done(null, existingUser);
-          } else {
-            //we dont have a user record with this ID, make a new record
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id }); //returns a promise
+
+      if (existingUser) {
+        //we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+      //we dont have a user record with this ID, make a new record
+      const user = await new User({ googleId: profile.id }).save(); //returns a promise
+      done(null, user);
     }
   )
 );
@@ -45,3 +42,45 @@ passport.use(
 // console.log('access Token', accessToken);
 // console.log('refresh Token', refreshToken);
 // console.log('profile: ', profile);
+
+//Example async/await arrow function
+// const fetchAlbums = async () => {
+//   const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
+//   const json = await res.json();
+//
+//   console.log(json);
+// };
+//
+// fetchAlbums();
+
+//passport/async arrow function prior to refactor
+// (accessToken, refreshToken, profile, done) => {
+//   User.findOne({ googleId: profile.id }) //returns a promise
+//     .then(existingUser => {
+//       if (existingUser) {
+//         //we already have a record with the given profile ID
+//         done(null, existingUser);
+//       } else {
+//         //we dont have a user record with this ID, make a new record
+//         new User({ googleId: profile.id })  //returns a promise
+//           .save()
+//           .then(user => done(null, user));
+//       }
+//     });
+// }
+
+//async code prior to further reduction
+//removed the else case in the production code above
+//adding a return statement inside the if statement eliminates the need for the else
+// async (accessToken, refreshToken, profile, done) => {
+//   const existingUser = await User.findOne({ googleId: profile.id }); //returns a promise
+//
+//   if (existingUser) {
+//     //we already have a record with the given profile ID
+//     done(null, existingUser);
+//   } else {
+//     //we dont have a user record with this ID, make a new record
+//     const user = await new User({ googleId: profile.id }).save(); //returns a promise
+//     done(null, user);
+//   }
+// }
